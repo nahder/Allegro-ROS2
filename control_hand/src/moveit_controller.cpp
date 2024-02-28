c2c373f3574e359451d0828e88ac94dba74fd6bf
Nader Ahmed <ndr.ahmed1@gmail.com>
2024-02-27 21:25:09 -0600

    testing moveit

--- control_hand/src/moveit_controller.cpp
+++ control_hand/src/moveit_controller.cpp
@@ -5,11 +5,38 @@
 #include "std_msgs/msg/string.hpp"
 #include "control_hand/srv/set_joints.hpp"
 #include "control_hand/srv/set_config.hpp"
-
-
 #include <moveit/move_group_interface/move_group_interface.h>
 #include "std_msgs/msg/float64_multi_array.hpp"
 #include "sensor_msgs/msg/joint_state.hpp"
+
+
+static double nonono1[] = {  //jt 5,9
+  -0.37471316618668479, 0.386, 0.174, -0.22753605719833834,
+  0.205, 1.636, 1.709, 0.227,
+  0.0, 1.636, 1.709, 0.32,
+  0.263, 0.410, 0.911, 1.5
+};
+
+
+static double nonono2[] = {  //jt 5,9
+  0.37181227113054078, 0.386, 0.174, -0.22753605719833834,
+  0.205, 1.636, 1.709, 0.227,
+  0.0, 1.636, 1.709, 0.32,
+  0.263, 0.410, 0.911, 1.5
+};
+
+double kp[] = {
+  500, 800, 900, 500,
+  500, 800, 900, 500,
+  500, 800, 900, 500,
+  1000, 700, 600, 600
+};
+double kd[] = {
+  25, 50, 55, 40,
+  25, 50, 55, 40,
+  25, 50, 55, 40,
+  50, 50, 50, 40
+};
 
 using namespace std::chrono_literals;
 static const std::string PLANNING_GROUP = "allegro_hand";
@@ -31,13 +58,12 @@ public:
         &MoveItPlanner::set_config_callback, this, std::placeholders::_1,
         std::placeholders::_2));
 
-
     joint_state_sub = create_subscription<sensor_msgs::msg::JointState>(
       "/joint_states", 10,
       std::bind(&MoveItPlanner::joint_state_callback, this, std::placeholders::_1));
 
     timer = this->create_wall_timer(
-      10ms, std::bind(&MoveItPlanner::timer_callback, this));
+      100ms, std::bind(&MoveItPlanner::timer_callback, this));
   }
 
 private:
@@ -67,7 +93,7 @@ private:
         this->get_logger(),
         "clamping to joint limits");
     }
-    move_group.setMaxVelocityScalingFactor(1.0);
+    move_group.setMaxVelocityScalingFactor(0.9);
     move_group.setMaxAccelerationScalingFactor(1.0);
     move_group.move();
     response->success = true;
@@ -75,8 +101,6 @@ private:
 
   void joint_state_callback(const sensor_msgs::msg::JointState & msg)
   {
-    //msg.position is in the order as they are on joint_states topic
-    //set q_des by msg.position names (joint_0...joint_15)
     q_des[0] = msg.position[0]; //joint_0
     q_des[1] = msg.position[2]; //joint_2
     q_des[2] = msg.position[1]; //joint_1
@@ -93,49 +117,70 @@ private:
     q_des[13] = msg.position[11]; //joint_11
     q_des[14] = msg.position[13]; //joint_13
     q_des[15] = msg.position[7]; //joint_7
+    pBHand->SetMotionType(eMotionType_JOINT_PD);
+    pBHand->SetGainsEx(kp, kd);
   }
 
   void set_config_callback(
     const std::shared_ptr<control_hand::srv::SetConfig::Request> request,
     std::shared_ptr<control_hand::srv::SetConfig::Response> response)
   {
-    auto success = move_group.setNamedTarget(request->name);
-    if (!success) {
-      RCLCPP_WARN(
-        this->get_logger(),
-        "invalid joint configuration name, moving to default position");
+    if (request->name == "nonono") {
       move_group.setNamedTarget("Home");
-    }
+      move_group.setMaxVelocityScalingFactor(1.0);
+      move_group.setMaxAccelerationScalingFactor(1.0);
+      move_group.move();
+      wrong_answer();
+      response->success = true;
 
-    move_group.setMaxVelocityScalingFactor(1.0);
-    move_group.setMaxAccelerationScalingFactor(1.0);
-    move_group.move();
-    response->success = true;
+    } else {
+      auto success = move_group.setNamedTarget(request->name);
+      if (!success) {
+        RCLCPP_WARN(
+          this->get_logger(),
+          "invalid joint configuration name, moving to default position");
+        move_group.setNamedTarget("Home");
+      }
+
+      move_group.setMaxVelocityScalingFactor(1.0);
+      move_group.setMaxAccelerationScalingFactor(1.0);
+      move_group.move();
+      response->success = true;
+    }
   }
 
   void timer_callback()
   {
-    pBHand->SetMotionType(eMotionType_JOINT_PD);
-    double kp[] = {
-      500, 800, 900, 500,
-      500, 800, 900, 500,
-      500, 800, 900, 500,
-      1000, 700, 600, 600
-    };
-    double kd[] = {
-      25, 50, 55, 40,
-      25, 50, 55, 40,
-      25, 50, 55, 40,
-      50, 50, 50, 40
-    };
-    pBHand->SetGainsEx(kp, kd);
+    // pBHand->SetMotionType(eMotionType_JOINT_PD);
+    // pBHand->SetGainsEx(kp, kd);
+  }
+
+  void wrong_answer()
+  {
+    //cycle between nonono1,2,3 two times
+    for (int i = 0; i < 3; i++) {
+      for (int j = 0; j < 16; j++) {
+        q_des[j] = nonono1[j];
+      }
+      if (pBHand) {pBHand->SetMotionType(eMotionType_JOINT_PD);}
+      pBHand->SetGainsEx(kp, kd);
+      usleep(0.2 * 1000000);
+
+      for (int j = 0; j < 16; j++) {
+        q_des[j] = nonono2[j];
+      }
+      if (pBHand) {pBHand->SetMotionType(eMotionType_JOINT_PD);}
+      pBHand->SetGainsEx(kp, kd);
+      usleep(0.2 * 1000000);
+    }
+    usleep(0.2 * 1000000);
   }
 };
 
 int main(int argc, char * argv[])
 {
   CreateBHandAlgorithm();
-  // OpenCAN();
+  OpenCAN();
   memset(&vars, 0, sizeof(vars));
   memset(q, 0, sizeof(q));
   memset(q_des, 0, sizeof(q_des));
@@ -147,71 +192,8 @@ int main(int argc, char * argv[])
   auto node = std::make_shared<MoveItPlanner>();
   rclcpp::spin(node);
   rclcpp::shutdown();
-  return 0;
 
-  // CloseCAN();
+  CloseCAN();
   DestroyBHandAlgorithm();
   return 0;
 }
-
-// static double rock[] = {
-//   -0.1194, //qdes[0]
-//   1.2068, //qdes[1]
-//   1.0, //qdes[2]
-//   1.4042, //qdes[3]
-//   -0.0093, //qdes[4]
-//   1.2481, //qdes[5]
-//   1.4073, //qdes[6]
-//   0.8163, //qdes[7]
-//   0.1116,   //qdes[8]
-//   1.2712, //qdes[9]
-//   1.3881, //qdes[10]
-//   1.0122, //qdes[11]
-//   0.6017, //qdes[12]
-//   0.2976, //qdes[13]
-//   0.9034, //qdes[14]
-//   0.7929};  //qdes[15]
-
-// construct the msg.position vector from q_des using the earlier mapping
-
-// static double msg_position[] = {
-//   q_des[0], // -0.1194
-//   q_des[2], // 1.0
-//   q_des[1], // 1.2068
-//   q_des[10], // 1.3881
-//   q_des[4], // -0.0093
-//   q_des[12], // 0.2976
-//   q_des[9], // 1.2712
-//   q_des[5], // 1.2481
-//   q_des[8], // 0.1116
-//   q_des[6], // 1.4073
-//   q_des[14], // 0.9034
-//   q_des[3], // 1.4042
-//   q_des[15], // 0.7929
-//   q_des[11], // 0.6017
-//   q_des[13], // 0.2976
-//   q_des[7] // 0.9034
-// };
-
-// static double msg_position_nums[] = {
-//   [-0.1194,
-//   1.0,
-//   1.2068,
-//   1.3881,
-//   -0.0093,
-//   0.2976,
-//   1.2712,
-//   1.2481,
-//   0.1116,
-//   1.4073,
-//   0.9034,
-//   1.4042,
-//   0.7929,
-//   0.6017,
-//   0.2976,
-//   0.9034]
-// };
-
-
-// make service call message with all 16 zeros
-// ros2 ser
