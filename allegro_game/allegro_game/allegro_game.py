55b4388e3e6fdccb5405df0acb313c99cd3c3c17
Nader Ahmed <ndr.ahmed1@gmail.com>
2024-02-28 19:06:01 -0600

    accumulating gestures over 5s

--- allegro_game/allegro_game/allegro_game.py
+++ allegro_game/allegro_game/allegro_game.py
@@ -1,145 +1,122 @@
 import rclpy
+import random
 from rclpy.node import Node
 from std_msgs.msg import String
 from std_srvs.srv import Trigger
-from control_hand.srv import Move
-import numpy as np 
+from control_hand.srv import Move, SetConfig
+import numpy as np
 from enum import Enum, auto
 from std_srvs.srv import Empty
-import time 
+from rclpy.callback_groups import MutuallyExclusiveCallbackGroup
+import time
+import threading
 import asyncio
-class State(Enum):
-    # NEUTRAL = auto()
-    IN_SEQUENCE = auto() #if the robot has gestures left
-    FINISHED_SEQUENCE = auto() #if the robot has no gestures left
-    
-#CURRENT GESTURES: 0: rock, 1: paper, 2: scissors,(3: okay,  9: nonono) 
+
+
+# CURRENT GESTURES: 0: rock, 1: paper, 2: scissors,(3: okay,  9: nonono)
 class AllegroGame(Node):
     def __init__(self):
-        super().__init__('allegro_game')
-        
-        self.gesture_sub = self.create_subscription(String, 'gesture', self.player_gesture_cb, 10) 
-        self.move_client = self.create_client(Move, 'controller/move')
-        self.reset_client = self.create_client(Trigger, 'controller/reset') 
-        self.delay_client = self.create_client(Empty, 'delay')
-        
-        gesture_dict = {0: "rock", 1: "paper", 2: "scissors"}
-        
-        self.player_gesture = None
-        self.moves = []
-        self.round = 5
+        super().__init__("allegro_game")
 
-        self.game_loop()
-    # Performs all the gestures in the moves list
-    # Send a reset move in between every gesture (config_idx = -100)
-    
-    def game_loop(self):
-        request = Move.Request()
-        self.moves = [-100, 0, -100, 1, -100, 2]
-        
-        for move in self.moves:
-            request.config_idx = move
-            self.move_client.call_async(request)
-            self.delay()
-            
-    def delay(self): 
-        self.future = self.delay_client.call_async(Empty.Request())
-        rclpy.spin_until_future_complete(self, self.future)
-        return self.future.result()
-                
+        self.cbgroup1 = MutuallyExclusiveCallbackGroup()
+        self.cbgroup2 = MutuallyExclusiveCallbackGroup()
+        self.defined_gestures = ["rock", "paper", "scissors", "okay", "3claw"]
+        self.gesture_sub = self.create_subscription(
+            String, "/gesture", self.player_gesture_cb, 10, callback_group=self.cbgroup1
+        )
+
+        self.timer = self.create_timer(
+            0.05, self.accumulate_player_gestures, callback_group=self.cbgroup2
+        )
+
+        self.set_config_srv = self.create_client(SetConfig, "set_config")
+        self.reset_client = self.create_client(Trigger, "controller/reset")
+        self.delay_client = self.create_client(Empty, "delay")
+
+        self.cur_player_gesture = None
+        self.prev_player_gesture = None
+
+        self.player_gestures = []
+        self.performed_gestures = []
+        self.round = 1
+
+        self.sampling_count = 0
+
+    # perform as many gestures as the round number
+    def perform_gestures(self):
+        request = SetConfig.Request()
+        indexes = random.sample(range(self.round), self.round)
+
+        for index in indexes:
+            request.name = self.defined_gestures[index]
+            self.set_config_srv.call_async(request)
+
+        self.performed_gestures = [self.defined_gestures[i] for i in indexes]
+
     def player_gesture_cb(self, msg):
-        self.player_gesture = msg.data 
-        self.get_logger().info('Player did: %s' % self.player_gesture)
-        
-    def demo(self): 
-        # Give robot a gesture
-        request = Move.Request()
-        request.config_idx = 1
-        self.move_client.call_async(request)
-        self.get_logger().info('Robot performed gesture 1, waiting for player to respond...')
-        
-        # Set a timer to check the player's gesture after 2 seconds
-        self.delay_timer = self.create_timer(timer_period_sec=2, callback=self.check_player_gesture)
-        
-    def check_player_gestures(self):
-        if self.player_gesture == "scissors":  # Assuming the player_gesture is a string "1" if correct
-            self.get_logger().info('Player did the same gesture')
-            request = Move.Request()
-            request.config_idx = 3  # "Yes" gesture
-            self.move_client.call_async(request)
-        else:
-            self.get_logger().info(f'Player did not do the same gesture, they did: {self.player_gesture} when the correct gesture was: 1')
-            request = Move.Request()
-            request.config_idx = 9  # "No no no" gesture
-            self.move_client.call_async(request)
-    
+        # self.get_logger().info("player gesture: %s" % msg.data)
+        self.cur_player_gesture = msg.data
 
-#algorithm which, knowing the correct sequence of gestures, will wait for the player to do the same sequence of gestures
-#after the player has done the same sequence of gestures, the robot will do a "yes" gesture and the game will continue  
-        
+    def play_round(self):
+        self.perform_gestures()
+        self.accumulate_player_gestures(3.0)
+
+    # accumulate the player's gestures (unique ones, in order) for period of time
+    # timer gets triggered every 0.02 seconds
+    # want to only accumulate for 3 seconds
+    # thus,
+    def accumulate_player_gestures(self):
+        if self.sampling_count < 100:
+            start_time = self.get_clock().now().to_msg()
+            start_time = start_time.sec + start_time.nanosec * 1e-9
+            cur_time = 0.0
+            cur_time = self.get_clock().now().to_msg()
+            cur_time = cur_time.sec + cur_time.nanosec * 1e-9
+
+            if self.cur_player_gesture != self.prev_player_gesture:
+                self.get_logger().info("adding gesture: %s" % self.cur_player_gesture)
+                self.player_gestures.append(self.cur_player_gesture)
+                self.prev_player_gesture = self.cur_player_gesture
+
+            print("player gestures", self.player_gestures)
+            self.sampling_count += 1
+        else:
+            self.get_logger().info("sampling done")
+
+    def reset_game(self):
+        pass
+
+
 def main(args=None):
     rclpy.init(args=args)
     allegro_game = AllegroGame()
     rclpy.spin(allegro_game)
     allegro_game.destroy_node()
     rclpy.shutdown()
-    
-#game starts with issuing one gesture to the hand 
-#then, give the player 3 seconds to do the same gesture...read from the gesture topic 
-
-#if the player does not do the same gesture, the robot will do a "no no no" gesture and the game will reset 
-    
-    #if the player does the same gesture, the robot will do a "yes" gesture and the game will continue
-
-#after 3 seconds, round will be incremented, and the robot will generate #round number of gestures 
 
 
+# game starts with issuing one gesture to the hand
+# then, give the player 3 seconds to do the same gesture...read from the gesture topic
+
+# if the player does not do the same gesture, the robot will do a "no no no" gesture and the game will reset
+
+# if the player does the same gesture, the robot will do a "yes" gesture and the game will continue
+
+# after 3 seconds, round will be incremented, and the robot will generate #round number of gestures
 
 
+# algorithm which, knowing the correct sequence of gestures, will wait for the player to do the same sequence of gestures
+# after the player has done the same sequence of gestures, the robot will do a "yes" gesture and the game will continue
 
 
-#define the configurations that lead to rock, paper, and scissors 
-#test them with the service call, then try making this node perform them with the
-#janky moveit controller
+# TO DO:
+# 1. perform a sequence of gestures
+# 2. perform gestures, give the player 3 seconds to do the same,
+# if the player did the same,
 
-# rock_joints = [-0.1194, 1.2068, 1.0, 1.4042, 
-                # -0.0093, 1.2481, 1.4073, 0.8163, 
-                # 0.1116, 1.2712, 1.3881, 1.0122, 
-                # 0.6017, 0.2976, 0.9034, 0.7929]
+# x. retrain mediapipe to be tight on rock, paper, scissors, and claw
 
-#service call:
-# ros2 service call /set_joints control_hand/srv/SetJoints {"joint_positions: [-0.1194, 1.2068, 1.0, 1.4042, -0.0093, 1.2481, 1.4073, 0.8163, 0.1116, 1.2712, 1.3881, 1.0122, 0.6017, 0.2976, 0.9034, 0.7929]}
-
-
-#reset all to zero service call
-# static double rock[] = {
-#   -0.1194, 1.2068, 1.0, 1.4042,
-#   -0.0093, 1.2481, 1.4073, 0.8163,
-#   0.1116, 1.2712, 1.3881, 1.0122,
-#   0.6017, 0.2976, 0.9034, 0.7929};
-
-# static double paper[] = {
-#   -0.1220, 0.4, 0.6, -0.0769,
-#   0.0312, 0.4, 0.6, -0.0,
-#   0.1767, 0.4, 0.6, -0.0528,
-#   0.5284, 0.3693, 0.8977, 0.4863};
-
-# static double scissors[] = {
-#   0.0885, 0.4, 0.6, -0.0704,
-#   0.0312, 0.4, 0.6, -0.0,
-#   0.1019, 1.2375, 1.1346,
-#   1.0244, 1.0, 0.6331, 1.3509, 1.0};
-
-# static double cust1[] = {
-#   0.0, 1.6, 0.426, 0.0,
-#   0.109, 0.0, 0.0, 0.0,
-#   0.048, 0.0, 0.0, 0.072,
-#   1.396, 0.018, 0.0, 1.089};
-
-# static double cust2[] = {
-#   -0.2124, -0.177, 0.148, 1.312,
-#   0.012, 0.323, 1.079, 1.335,
-#   -0.144, 0.943, 0.335, 1.458,
-#   0.323, 0.182, 0.702, 0.297
-# };
-
+# def delay(self):
+#     self.future = self.delay_client.call_async(Empty.Request())
+#     rclpy.spin_until_future_complete(self, self.future)
+#     return self.future.result()
