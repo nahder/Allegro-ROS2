e1fd7b228ca48101d630198fd51af2410d3075e8
Nader Ahmed <ndr.ahmed1@gmail.com>
2024-03-05 17:22:08 -0600

    5 ROUNDS WORKING

--- allegro_game/allegro_game/allegro_game.py
+++ allegro_game/allegro_game/allegro_game.py
@@ -63,55 +63,70 @@ class AllegroGame(Node):
 
         self.performed_gestures = [self.defined_gestures[i] for i in indexes]
 
+    # maybe make a diff timer that sets the robot's gestures so
+    # that we can control how often its updated (this is just the gesture publishing timer)
     def player_gesture_cb(self, msg):
-        self.stability_count += 1
-        if self.stability_count > 20:
+        # stability_count_thresh = seconds / 0.02 (gestures published every 0.02s)
+        seconds = 2.5
+        stability_count_thresh = seconds / 0.02
+        self.stability_count += 1  # gestures published every 0.02s
+        if self.stability_count > stability_count_thresh:  # 3 seconds
             self.cur_player_gesture = msg.data
             self.stability_count = 0
 
     def game_loop(self):
-        if self.play_round and self.round < 3:
+        if self.play_round and self.round < 5:
             self.round_logger.reset()
             self.round_logger.log("Round: %d" % self.round)
             self.player_gestures = []
             self.prev_player_gesture = None
+            self.cur_player_gesture = None
             self.perform_gestures()
             self.round_logger.log("robot gestures: %s" % self.performed_gestures)
             self.sampling_count = 0  # begins accumulating player gestures
             self.play_round = False
 
     def accumulate_player_gestures(self):
-        if self.sampling_count < 120:  # accumulating for 5 seconds
+        # accumulate player_gestures called every 0.05s
+        # to wait for 5 seconds, we need 100 calls
+        # to wait for 10 seconds, we need 200 calls
+        # 400 counts = 20 seconds
+        # 300 counts = 15 seconds
+        # 500 counts = 25 seconds
+        # i dont think these count to seconds are accurate
+        if self.sampling_count < 500:
             self.round_logger.log("Start copying the robot's gestures!")
 
             if self.cur_player_gesture != self.prev_player_gesture:
                 self.player_gestures.append(self.cur_player_gesture)
+                self.round_logger.log("Added gesture: %s" % self.cur_player_gesture)
                 self.prev_player_gesture = self.cur_player_gesture
 
             self.sampling_count += 1
 
+        # change this so that once the player is recognized to have all the gestures
+        # in the sequence, the game will move on to the next round
+
         else:  # 5 seconds have passed
             self.round_logger.log("player gestures: %s" % self.player_gestures)
 
+            # if the player's gestures match the robot's gestures
             if self.performed_gestures == self.player_gestures:
                 self.round += 1
-                self.play_round = True
-                self.prev_player_gesture = None
                 self.round_logger.log("You did it! Next round!")
-                self.player_gestures = []
+                self.play_round = True
 
+            # otherwise, nonono and then reset the game
             else:
                 request = SetConfig.Request()
                 request.name = "nonono"
                 self.set_config_srv.call_async(request)
-                self.round_logger.reset()
                 self.round = 1
-                self.play_round = True
                 self.round_logger.log("You failed!")
                 self.round_logger.log("robot gestures: %s" % self.performed_gestures)
                 self.round_logger.log("player gestures: %s" % self.player_gestures)
                 self.round_logger.log("Restarting...")
-                self.player_gestures = []
+                self.play_round = True
 
 
 class ResetLogger:
@@ -134,3 +149,6 @@ def main(args=None):
     rclpy.spin(allegro_game)
     allegro_game.destroy_node()
     rclpy.shutdown()
+
+
+# need to add some indication as to when the player has completed 1 gesture out of the sequence
--- control_hand/src/moveit_controller.cpp
+++ control_hand/src/moveit_controller.cpp
@@ -211,7 +211,7 @@ private:
 int main(int argc, char * argv[])
 {
   CreateBHandAlgorithm();
-  OpenCAN();
+  // OpenCAN();
   memset(&vars, 0, sizeof(vars));
   memset(q, 0, sizeof(q));
   memset(q_des, 0, sizeof(q_des));
@@ -224,7 +224,7 @@ int main(int argc, char * argv[])
   rclcpp::spin(node);
   rclcpp::shutdown();
 
-  CloseCAN();
+  // CloseCAN();
   DestroyBHandAlgorithm();
   return 0;
 }
